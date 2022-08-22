94019b74f84ce140cc3fbc1e0fae291bfa87d82a
Lucas Daniel <101567841+lucasdanielzx@users.noreply.github.com>
2022-08-22 17:24:46 -0300

    Add files via upload

--- ConexaoTeste/Cadastrar.php
+++ ConexaoTeste/Cadastrar.php
@@ -11,12 +11,39 @@
 <body>
     <h1>Cadastrar</h1>
     <?php
-        $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+        $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT); //receber dados
 
         if(!empty($dados['CadUser'])){
             var_dump ($dados);
+            $empty_input = false;
+
+            $dados = array_map('trim', $dados);
+
+            if(in_array("", $dados)) {
+                $empty_input = true;
+                echo "<p style = 'color: red'>ERRO: Necessário preencher todos os campos.</p>";
+            } else if (!filter_var($dados['email'], FILTER_VALIDATE_EMAIL)){
+                $empty_input = true;
+                echo "<p style = 'color: red'>ERRO: Necessário preencher com email válido.</p>";
+            }
+            
+            if (!$empty_input) {
+                $query_usuario = "INSERT INTO usuarios (nome, email) VALUES (:nome, :email)";
+                $cad_usuario = $conn->prepare($query_usuario);
+                $cad_usuario->bindParam(':nome', $dados['nome'], PDO::PARAM_STR);
+                $cad_usuario->bindParam(':email', $dados['email'], PDO::PARAM_STR);
+                $cad_usuario->execute();
+
+                if ($cad_usuario->rowCount()){
+                    echo "<p style = 'color: green'>SUCESSO: Usuário cadastrado!</p>";
+                } else {
+                    echo "<p style = 'color: red'>ERRO: Usuário não cadastrado.</p>";
+                }
+            }
+            
         }
     ?>
+
     <form name = "cad-usuario" method = "post" action="">
         <label>Nome: </label>
         <input type = text name = "nome" id = "nome" placeholder = "nome completo"><br><br>
